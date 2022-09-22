7cb92a87df391279238fb491987740d8e409d9f8
Jordan <jwald3@brockport.edu>
2022-09-22 03:12:34 -0400

    factored out aggregation function from def stats

--- pages/stats/players/defense.tsx
+++ pages/stats/players/defense.tsx
@@ -176,6 +176,90 @@ const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
     ]);
 
+    const aggregateStats = (dataframe: IPlayerSeason[]) => {
+        let teamsMap = new Map();
+
+        for (let obj in dataframe) {
+            if (teamsMap.get(dataframe[obj].gsis_id)) {
+                let currentObj = teamsMap.get(dataframe[obj].gsis_id);
+                let newObj = {
+                    player_id: dataframe[obj].player_id,
+                    week_count:
+                        Number.parseInt(currentObj.week_count.toString()) +
+                        Number.parseInt(dataframe[obj].week_count.toString()),
+                    interception:
+                        Number.parseInt(currentObj.interception.toString()) +
+                        Number.parseInt(dataframe[obj].interception.toString()),
+                    int_return_yards:
+                        Number.parseInt(
+                            currentObj.int_return_yards.toString()
+                        ) +
+                        Number.parseInt(
+                            dataframe[obj].int_return_yards.toString()
+                        ),
+                    int_return_touchdown:
+                        Number.parseInt(
+                            currentObj.int_return_touchdown.toString()
+                        ) +
+                        Number.parseInt(
+                            dataframe[obj].int_return_touchdown.toString()
+                        ),
+                    passes_defended:
+                        Number.parseInt(currentObj.passes_defended.toString()) +
+                        Number.parseInt(
+                            dataframe[obj].passes_defended.toString()
+                        ),
+                    fumbles_forced:
+                        Number.parseInt(currentObj.fumbles_forced.toString()) +
+                        Number.parseInt(
+                            dataframe[obj].fumbles_forced.toString()
+                        ),
+                    sack:
+                        Number.parseInt(currentObj.sack.toString()) +
+                        Number.parseInt(dataframe[obj].sack.toString()),
+                    half_sack:
+                        Number.parseInt(currentObj.half_sack.toString()) +
+                        Number.parseInt(dataframe[obj].half_sack.toString()),
+                    solo_tackles:
+                        Number.parseInt(currentObj.solo_tackles.toString()) +
+                        Number.parseInt(dataframe[obj].solo_tackles.toString()),
+                    assist_tackls:
+                        Number.parseInt(currentObj.assist_tackls.toString()) +
+                        Number.parseInt(
+                            dataframe[obj].assist_tackls.toString()
+                        ),
+                    tackle_with_assist:
+                        Number.parseInt(
+                            currentObj.tackle_with_assist.toString()
+                        ) +
+                        Number.parseInt(
+                            dataframe[obj].tackle_with_assist.toString()
+                        ),
+                    tackles_for_loss:
+                        Number.parseInt(
+                            currentObj.tackles_for_loss.toString()
+                        ) +
+                        Number.parseInt(
+                            dataframe[obj].tackles_for_loss.toString()
+                        ),
+                    qb_hits:
+                        Number.parseInt(currentObj.qb_hits.toString()) +
+                        Number.parseInt(dataframe[obj].qb_hits.toString()),
+                    game_id_db: currentObj.game_id_db,
+                    team_abbr: currentObj.team_abbr,
+                    position: currentObj.position,
+                    gsis_id: currentObj.gsis_id,
+                };
+                teamsMap.set(currentObj.gsis_id, newObj);
+            } else {
+                teamsMap.set(dataframe[obj].gsis_id, {
+                    ...dataframe[obj],
+                });
+            }
+        }
+        return Array.from(teamsMap.values());
+    };
+
     useEffect(() => {
         if (query.weeks !== undefined && query.weeks !== "") {
             const selectedWeeks = (query.weeks as string)
@@ -191,185 +275,17 @@ const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
     }, []);
 
     useEffect(() => {
-        let teamsMap = new Map();
-
-        for (let obj in props.teams) {
-            if (teamsMap.get(props.teams[obj].gsis_id)) {
-                let currentObj = teamsMap.get(props.teams[obj].gsis_id);
-                let newObj = {
-                    player_id: props.teams[obj].player_id,
-                    week_count:
-                        Number.parseInt(currentObj.week_count.toString()) +
-                        Number.parseInt(props.teams[obj].week_count.toString()),
-                    interception:
-                        Number.parseInt(currentObj.interception.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].interception.toString()
-                        ),
-                    int_return_yards:
-                        Number.parseInt(
-                            currentObj.int_return_yards.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].int_return_yards.toString()
-                        ),
-                    int_return_touchdown:
-                        Number.parseInt(
-                            currentObj.int_return_touchdown.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].int_return_touchdown.toString()
-                        ),
-                    passes_defended:
-                        Number.parseInt(currentObj.passes_defended.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].passes_defended.toString()
-                        ),
-                    fumbles_forced:
-                        Number.parseInt(currentObj.fumbles_forced.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].fumbles_forced.toString()
-                        ),
-                    sack:
-                        Number.parseInt(currentObj.sack.toString()) +
-                        Number.parseInt(props.teams[obj].sack.toString()),
-                    half_sack:
-                        Number.parseInt(currentObj.half_sack.toString()) +
-                        Number.parseInt(props.teams[obj].half_sack.toString()),
-                    solo_tackles:
-                        Number.parseInt(currentObj.solo_tackles.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].solo_tackles.toString()
-                        ),
-                    assist_tackls:
-                        Number.parseInt(currentObj.assist_tackls.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].assist_tackls.toString()
-                        ),
-                    tackle_with_assist:
-                        Number.parseInt(
-                            currentObj.tackle_with_assist.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].tackle_with_assist.toString()
-                        ),
-                    tackles_for_loss:
-                        Number.parseInt(
-                            currentObj.tackles_for_loss.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].tackles_for_loss.toString()
-                        ),
-                    qb_hits:
-                        Number.parseInt(currentObj.qb_hits.toString()) +
-                        Number.parseInt(props.teams[obj].qb_hits.toString()),
-                    game_id_db: currentObj.game_id_db,
-                    team_abbr: currentObj.team_abbr,
-                    position: currentObj.position,
-                    gsis_id: currentObj.gsis_id,
-                };
-                teamsMap.set(currentObj.gsis_id, newObj);
-            } else {
-                teamsMap.set(props.teams[obj].gsis_id, {
-                    ...props.teams[obj],
-                });
-            }
-        }
-        const reducedTeams = Array.from(teamsMap.values());
+        const reducedTeams = aggregateStats(props.teams);
 
         setAggTeams(reducedTeams);
     }, []);
 
     useEffect(() => {
-        let teamsMap = new Map();
-
         const filteredTeams = props.teams.filter((team) =>
             weekFilter.includes(Number.parseInt(team.week.toString()))
         );
 
-        for (let obj in filteredTeams) {
-            if (teamsMap.get(filteredTeams[obj].gsis_id)) {
-                let currentObj = teamsMap.get(filteredTeams[obj].gsis_id);
-                let newObj = {
-                    player_id: props.teams[obj].player_id,
-                    week_count:
-                        Number.parseInt(currentObj.week_count.toString()) +
-                        Number.parseInt(props.teams[obj].week_count.toString()),
-                    interception:
-                        Number.parseInt(currentObj.interception.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].interception.toString()
-                        ),
-                    int_return_yards:
-                        Number.parseInt(
-                            currentObj.int_return_yards.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].int_return_yards.toString()
-                        ),
-                    int_return_touchdown:
-                        Number.parseInt(
-                            currentObj.int_return_touchdown.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].int_return_touchdown.toString()
-                        ),
-                    passes_defended:
-                        Number.parseInt(currentObj.passes_defended.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].passes_defended.toString()
-                        ),
-                    fumbles_forced:
-                        Number.parseInt(currentObj.fumbles_forced.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].fumbles_forced.toString()
-                        ),
-                    sack:
-                        Number.parseInt(currentObj.sack.toString()) +
-                        Number.parseInt(props.teams[obj].sack.toString()),
-                    half_sack:
-                        Number.parseInt(currentObj.half_sack.toString()) +
-                        Number.parseInt(props.teams[obj].half_sack.toString()),
-                    solo_tackles:
-                        Number.parseInt(currentObj.solo_tackles.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].solo_tackles.toString()
-                        ),
-                    assist_tackls:
-                        Number.parseInt(currentObj.assist_tackls.toString()) +
-                        Number.parseInt(
-                            props.teams[obj].assist_tackls.toString()
-                        ),
-                    tackle_with_assist:
-                        Number.parseInt(
-                            currentObj.tackle_with_assist.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].tackle_with_assist.toString()
-                        ),
-                    tackles_for_loss:
-                        Number.parseInt(
-                            currentObj.tackles_for_loss.toString()
-                        ) +
-                        Number.parseInt(
-                            props.teams[obj].tackles_for_loss.toString()
-                        ),
-                    qb_hits:
-                        Number.parseInt(currentObj.qb_hits.toString()) +
-                        Number.parseInt(props.teams[obj].qb_hits.toString()),
-                    game_id_db: currentObj.game_id_db,
-                    team_abbr: currentObj.team_abbr,
-                    position: currentObj.position,
-                    gsis_id: currentObj.gsis_id,
-                };
-                teamsMap.set(currentObj.gsis_id, newObj);
-            } else {
-                teamsMap.set(filteredTeams[obj].gsis_id, {
-                    ...filteredTeams[obj],
-                });
-            }
-        }
-        const reducedTeams = Array.from(teamsMap.values());
+        const reducedTeams = aggregateStats(filteredTeams);
 
         setAggTeams(reducedTeams);
     }, [weekFilter]);
