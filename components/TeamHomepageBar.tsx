5cdc3dcb20a3d5f3050ff94323eec6b06d1ec5c9
Jordan <jwald3@brockport.edu>
2022-10-14 17:36:04 -0400

    Added styles for team link bar and team homepage bar

--- components/TeamHomepageBar.tsx
+++ components/TeamHomepageBar.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import styles from "../styles/TeamHomepageBar.module.scss";
 
 interface BarProps {
     teamName: string;
@@ -9,39 +9,9 @@ interface BarProps {
 
 const TeamHomepageBar: React.FunctionComponent<BarProps> = ({ ...props }) => {
     return (
-        <div
-            className="teamLinkBar"
-            style={{
-                width: "90%",
-                maxWidth: "2000px",
-                display: "flex",
-                justifyContent: "space-between",
-                marginLeft: "auto",
-                marginRight: "auto",
-                height: "10vh",
-                padding: "2%",
-                backgroundColor: "#f3f4f8",
-                marginBottom: "2%",
-                alignItems: "center",
-                boxShadow: "0px 0.3em 0.3em 0.3em rgba(0, 0, 0, 0.25)",
-                textTransform: "uppercase",
-            }}
-        >
+        <div className={styles.teamLinkBar}>
             <div style={{ display: "flex", flexDirection: "column" }}>
-                <div
-                    className="teamLinkHeader"
-                    style={{
-                        height: "100%",
-                        width: "100%",
-                        display: "flex",
-                        alignItems: "center",
-                        fontSize: "1.5em",
-                        fontWeight: "bold",
-                        color: "#282a3a",
-                    }}
-                >
-                    {props.teamName}
-                </div>
+                <div className={styles.teamLinkHeader}>{props.teamName}</div>
                 <div>
                     {props.record}, {props.divisionStanding} in{" "}
                     {props.divisionName}
--- components/TeamLinkBar.tsx
+++ components/TeamLinkBar.tsx
@@ -1,58 +1,15 @@
+import styles from "../styles/TeamLinkBar.module.scss";
 import { useRouter } from "next/router";
 
 const TeamLinkBar: React.FunctionComponent = () => {
     const router = useRouter();
 
     return (
-        <div
-            className="teamLinkBar"
-            style={{
-                width: "90%",
-                maxWidth: "2000px",
-                display: "flex",
-                justifyContent: "space-between",
-                marginLeft: "auto",
-                marginRight: "auto",
-                height: "10vh",
-                padding: "2%",
-                backgroundColor: "#f3f4f8",
-                marginBottom: "2%",
-                alignItems: "center",
-                boxShadow: "0px 0.3em 0.3em 0.3em rgba(0, 0, 0, 0.25)",
-                textTransform: "uppercase",
-            }}
-        >
-            <div
-                className="teamLinkHeader"
-                style={{
-                    height: "100%",
-                    width: "65%",
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "1.5em",
-                    fontWeight: "bold",
-                    color: "#282a3a",
-                }}
-            >
-                Team Links
-            </div>
-            <div
-                className="teamLinks"
-                style={{
-                    height: "100%",
-                    fontSize: "1em",
-                    fontWeight: "bold",
-                    width: "35%",
-                    maxWidth: "500px",
-                    display: "flex",
-                    gap: "2%",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    color: "#777986",
-                }}
-            >
+        <div className={styles.teamLinkBar}>
+            <div className={styles.teamLinkHeader}>Team Links</div>
+            <div className={styles.teamLinks}>
                 <div
-                    className="teamLink"
+                    className={styles.teamLink}
                     onClick={() =>
                         router.replace({
                             pathname: "/teams/[team]",
@@ -61,12 +18,11 @@ const TeamLinkBar: React.FunctionComponent = () => {
                             },
                         })
                     }
-                    style={{ cursor: "pointer" }}
                 >
                     Home
                 </div>
                 <div
-                    className="teamLink"
+                    className={styles.teamLink}
                     onClick={() =>
                         router.replace({
                             pathname: "/teams/[team]/usage",
@@ -75,12 +31,11 @@ const TeamLinkBar: React.FunctionComponent = () => {
                             },
                         })
                     }
-                    style={{ cursor: "pointer" }}
                 >
                     Usage Rates
                 </div>
                 <div
-                    className="teamLink"
+                    className={styles.teamLink}
                     onClick={() =>
                         router.replace({
                             pathname: "/teams/[team]/personnel",
@@ -89,12 +44,11 @@ const TeamLinkBar: React.FunctionComponent = () => {
                             },
                         })
                     }
-                    style={{ cursor: "pointer" }}
                 >
                     Personnel
                 </div>
                 <div
-                    className="teamLink"
+                    className={styles.teamLink}
                     onClick={() =>
                         router.replace({
                             pathname: "/teams/[team]/snap_counts/offense",
@@ -103,7 +57,6 @@ const TeamLinkBar: React.FunctionComponent = () => {
                             },
                         })
                     }
-                    style={{ cursor: "pointer" }}
                 >
                     Snap Counts
                 </div>
