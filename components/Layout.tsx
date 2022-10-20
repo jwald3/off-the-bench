f22e3a7a50d3fc60681a83006d07a94324fed6af
Jordan <jwald3@brockport.edu>
2022-10-19 20:34:13 -0400

    fixed issue with layout ambiguity

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,12 @@
 import DrawerAppBar from "./Appbar";
+import React, { ReactNode } from "react";
 
-const Layout = ({ children }) => {
+interface Props {
+    children?: ReactNode;
+    // any props that come into the component
+}
+
+const Layout = ({ children, ...props }: Props) => {
     return (
         <div className="content">
             <DrawerAppBar />
