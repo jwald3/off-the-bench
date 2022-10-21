1a9e64f6a3f9e9b04063a077077a413bb85eb4b7
Jordan <jwald3@brockport.edu>
2022-10-21 01:36:55 -0400

    restyled home page in stylesheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Footer from "../components/Footer";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
     return (
         <div>
-            <div className={styles.container}>
+            <div className={styles.homeContainer}>
                 <Head>
                     <title>Off The Bench</title>
                     <meta
@@ -17,51 +16,11 @@ const Home: NextPage = () => {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
 
-                <div style={{ backgroundColor: "#d9d9d9", paddingTop: "1%" }}>
-                    <div
-                        className="banner"
-                        style={{
-                            position: "relative",
-                            backgroundImage:
-                                "url(https://images.pexels.com/photos/264300/pexels-photo-264300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
-                            backgroundSize: "auto",
-                            backgroundPosition: "right",
-                            backgroundRepeat: "no-repeat",
-                            maxWidth: "1800px",
-                            maxHeight: "450px",
-                            height: "50vh",
-                            margin: "0 auto",
-                            marginTop: "2%",
-                            borderRadius: "1em",
-                            backgroundColor: "#d9d9d9",
-                            boxShadow:
-                                "0px 0.15em 0.15em 0.15em rgba(0, 0, 0, 0.25);",
-                            width: "90%",
-                        }}
-                    >
-                        <div
-                            className="clipped"
-                            style={{
-                                width: "60%",
-                                height: "100%",
-                                background: "#f3f4f8",
-                                clipPath:
-                                    "polygon(0 0, 100% 0%, 80% 100%, 0% 100%)",
-                                borderRadius: "1em 0 0 1em",
-                                display: "flex",
-                                alignItems: "center",
-                            }}
-                        >
-                            <div>
-                                <div
-                                    className="logo"
-                                    style={{
-                                        width: "100%",
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        paddingBottom: "2%",
-                                    }}
-                                >
+                <div className={styles.homeBodyContainer}>
+                    <div className={styles.banner}>
+                        <div className={styles.clipped}>
+                            <div className={styles.clippedInner}>
+                                <div className={styles.logo}>
                                     <Image
                                         src="/Logo-Purple-No-Background.png"
                                         alt="logo"
@@ -69,16 +28,7 @@ const Home: NextPage = () => {
                                         height={130}
                                     />
                                 </div>
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        width: "60%",
-                                        maxWidth: "800px",
-                                        textAlign: "center",
-                                        margin: "auto",
-                                        color: "#494252",
-                                    }}
-                                >
+                                <div className={styles.textBlurb}>
                                     <span>
                                         Off The Bench provides player and team
                                         stats for the NFL to help you gain an
@@ -89,213 +39,63 @@ const Home: NextPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            width: "100%",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            height: "20vh",
-                            fontSize: "2em",
-                            color: "#494252",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        Our Features
-                    </div>
-                    <div style={{ paddingBottom: "10%" }}>
-                        <div
-                            style={{
-                                display: "flex",
-                                width: "90%",
-                                maxWidth: "1800px",
-                                justifyContent: "space-around",
-                                height: "40vh",
-                                padding: "2%",
-                                margin: "auto",
-                                borderRadius: "1em",
-                                backgroundColor: "#f3f4f8",
-                                boxShadow:
-                                    "0px 0.15em 0.15em 0.15em rgba(0, 0, 0, 0.25);",
-                                alignItems: "center",
-                            }}
-                        >
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    borderRadius: "1em",
-                                    backgroundColor: "#f3f4f8",
-                                    boxShadow:
-                                        "0px .3em .3em .3em rgba(0, 0, 0, 0.25);",
-                                    maxWidth: "400px",
-                                    width: "25%",
-                                    height: "100%",
-                                }}
-                            >
-                                <div
-                                    className="front-page-img-container"
-                                    style={{
-                                        height: "50%",
-                                        display: "block",
-                                    }}
-                                >
+                    <div className={styles.featuresHeader}>Our Features</div>
+                    <div className={styles.featuresArea}>
+                        <div className={styles.featuresContainer}>
+                            <div className={styles.singleFeature}>
+                                <div className={styles.frontPageImgContainer}>
                                     <Image
-                                        className="front-page-img"
+                                        className={styles.frontPageImg}
                                         quality={100}
                                         src="/filters.png"
                                         layout="responsive"
                                         width={400}
                                         height={175}
                                         alt="Team Page Screenshot"
-                                        style={{
-                                            borderRadius: "1em 1em 0 0",
-                                        }}
                                     />
                                 </div>
-                                <div
-                                    style={{
-                                        height: "50%",
-                                        backgroundColor:
-                                            "rgba(243, 244, 248, 0.5)",
-                                        width: "100%",
-                                        maxWidth: "100%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        flexWrap: "wrap",
-                                        borderRadius: "inherit",
-                                    }}
-                                >
-                                    <span
-                                        style={{
-                                            width: "75%",
-                                            fontSize: "1em",
-                                            textAlign: "center",
-                                            color: "#777986",
-                                        }}
-                                    >
+                                <div className={styles.featureText}>
+                                    <span>
                                         Each stat on the site can be filtered by
                                         week and down to let you specify your
                                         queries.
                                     </span>
                                 </div>
                             </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    borderRadius: "1em",
-                                    backgroundColor: "#f3f4f8",
-                                    boxShadow:
-                                        "0px .3em .3em .3em rgba(0, 0, 0, 0.25);",
-                                    maxWidth: "400px",
-                                    width: "25%",
-                                    height: "100%",
-                                }}
-                            >
-                                <div
-                                    className="front-page-img-container"
-                                    style={{
-                                        height: "50%",
-                                        display: "block",
-                                    }}
-                                >
+                            <div className={styles.singleFeature}>
+                                <div className={styles.frontPageImgContainer}>
                                     <Image
-                                        className="front-page-img"
+                                        className={styles.frontPageImg}
                                         quality={100}
                                         src="/personnel.png"
                                         layout="responsive"
-                                        height={175}
                                         width={400}
+                                        height={175}
                                         alt="Team Page Screenshot"
-                                        style={{
-                                            borderRadius: "1em 1em 0 0",
-                                        }}
                                     />
                                 </div>
-                                <div
-                                    style={{
-                                        height: "50%",
-                                        backgroundColor:
-                                            "rgba(243, 244, 248, 0.7)",
-                                        width: "100%",
-                                        maxWidth: "100%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        flexWrap: "wrap",
-                                        borderRadius: "inherit",
-                                    }}
-                                >
-                                    <span
-                                        style={{
-                                            width: "75%",
-                                            fontSize: "1em",
-                                            textAlign: "center",
-                                            color: "#777986",
-                                        }}
-                                    >
+                                <div className={styles.featureText}>
+                                    <span>
                                         Team tendencies by down and personnel
                                         grouping help you predict in-game
                                         decisions.
                                     </span>
                                 </div>
                             </div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    borderRadius: "1em",
-                                    backgroundColor: "#f3f4f8",
-                                    boxShadow:
-                                        "0px .3em .3em .3em rgba(0, 0, 0, 0.25);",
-                                    maxWidth: "400px",
-                                    width: "25%",
-                                    height: "100%",
-                                }}
-                            >
-                                <div
-                                    className="front-page-img-container"
-                                    style={{
-                                        display: "block",
-                                    }}
-                                >
+                            <div className={styles.singleFeature}>
+                                <div className={styles.frontPageImgContainer}>
                                     <Image
-                                        className="front-page-img"
+                                        className={styles.frontPageImg}
                                         quality={100}
                                         src="/receiving.png"
                                         layout="responsive"
-                                        height={"100%"}
                                         width={400}
+                                        height={175}
                                         alt="Team Page Screenshot"
-                                        style={{
-                                            borderRadius: "1em 1em 0 0",
-                                        }}
                                     />
                                 </div>
-                                <div
-                                    style={{
-                                        height: "50%",
-                                        backgroundColor:
-                                            "rgba(243, 244, 248, 0.7)",
-                                        width: "100%",
-                                        maxWidth: "100%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        flexWrap: "wrap",
-                                        borderRadius: "inherit",
-                                    }}
-                                >
-                                    <span
-                                        style={{
-                                            width: "75%",
-                                            fontSize: "1em",
-                                            textAlign: "center",
-                                            color: "#777986",
-                                        }}
-                                    >
+                                <div className={styles.featureText}>
+                                    <span>
                                         Advanced stats like air yards and red
                                         zone usage offer you prime targets for
                                         regression.
