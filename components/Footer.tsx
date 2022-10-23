924fc6ee7fc6ff299201846a4b31b6c9c1a90bb6
Jordan <jwald3@brockport.edu>
2022-10-23 03:35:09 -0400

    Added stylesheet for footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/Footer.module.scss";
 
 const Footer = () => {
     return (
-        <div
-            style={{
-                width: "100%",
-                height: "30vh",
-                backgroundColor: "#494252",
-                alignItems: "center",
-                display: "flex",
-                boxShadow: " 0.5em 0.5em 0.5em .5em rgba(0, 0, 0, 0.25)",
-            }}
-        >
-            <div
-                style={{
-                    flex: "1",
-                    display: "flex",
-                    height: "90%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingLeft: "5%",
-                    paddingRight: "5%",
-                    paddingTop: "1%",
-                    borderRight: "1px solid #f3f4f8",
-                    color: "#f3f4f8",
-                    flexDirection: "column",
-                }}
-            >
+        <div className={styles.footerContainer}>
+            <div className={styles.leftFooter}>
                 <Image
                     src="/logo1.png"
                     alt="Logo"
@@ -38,46 +16,16 @@ const Footer = () => {
                 />
                 <div>Off The Bench, 2022</div>
             </div>
-            <div
-                style={{
-                    flex: "5",
-                    display: "flex",
-                    justifyContent: "space-around",
-                    padding: "2% 5%",
-                    height: "90%",
-                }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        color: "#d2d4da",
-                        height: "100%",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <div style={{ fontWeight: "bold", color: "#f3f4f8" }}>
-                        Teams
-                    </div>
+            <div className={styles.rightFooter}>
+                <div className={styles.linkColumn}>
+                    <div className={styles.linkHeader}>Teams</div>
                     <Link href="/teams">All Teams</Link>
-                    <div style={{ fontWeight: "bold", color: "#f3f4f8" }}>
-                        Team Stats
-                    </div>
+                    <div className={styles.linkHeader}>Team Stats</div>
                     <Link href="/stats/teams/offense">Basic Offense</Link>
                     <Link href="/stats/teams/defense">Basic Defense</Link>
                 </div>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        color: "#d2d4da",
-                        height: "100%",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <div style={{ fontWeight: "bold", color: "#f3f4f8" }}>
-                        Player Stats
-                    </div>
+                <div className={styles.linkColumn}>
+                    <div className={styles.linkHeader}>Player Stats</div>
                     <Link href="/stats/players/offense">Basic Offense</Link>
                     <Link href="/stats/players/defense">Basic Defense</Link>
                     <Link href="/stats/players/receiving">
@@ -85,16 +33,8 @@ const Footer = () => {
                     </Link>
                     <div>—</div>
                 </div>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        color: "#d2d4da",
-                        height: "100%",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <div style={{ fontWeight: "bold", color: "#f3f4f8" }}>
+                <div className={styles.linkColumn}>
+                    <div className={styles.linkHeader}>
                         Over The Bench Links
                     </div>
                     <Link href="/">Home</Link>
