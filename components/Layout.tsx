230f7587663ec17d350a660819cc35d18769d5b6
Jordan <jwald3@brockport.edu>
2022-10-25 00:22:53 -0400

    Removed unneeded lines fom files

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,6 @@ import Footer from "./Footer";
 
 interface Props {
     children?: ReactNode;
-    // any props that come into the component
 }
 
 const Layout = ({ children, ...props }: Props) => {
