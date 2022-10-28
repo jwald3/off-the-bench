ac67ce82d40f9dacc4f447d99777c579fd7af0dc
Jordan <jwald3@brockport.edu>
2022-10-28 17:14:21 -0400

    Added progress bar loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
+            <NextNProgress color="#1da1f2" />
             <Component {...pageProps} />
         </Layout>
     );
