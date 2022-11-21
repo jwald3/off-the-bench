5f09cfc4a637be3728607e4e09fe351b27a7973d
Jordan <jwald3@brockport.edu>
2022-11-21 02:51:21 -0500

    Refactored /[team]/personnel page

--- pages/stats/players/dummy.tsx
+++ pages/stats/players/dummy.tsx
@@ -13,11 +13,13 @@ import {
 } from "../../../data/tableColumns";
 import Head from "next/head";
 import SelectorTray from "../../../components/SelectorTray";
-import styles from "../../../styles/UsagePage.module.scss";
+import styles from "../../../styles/PersonnelPage.module.scss";
 import { parseBigInt, regSeasonWeeks } from "../../../data/globalVars";
-import { ITeamFormationStats } from "../../../ts/interfaces/teamInterfaces";
+import {
+    ITeamFormationStats,
+    ITeamPersonnelStats,
+} from "../../../ts/interfaces/teamInterfaces";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { IPlayerUsageStats } from "../../../ts/interfaces/playerInterfaces";
 import UsageInfo from "../../../components/UsageInfo";
 import TeamLinkFooter from "../../../components/TeamFooter";
 
@@ -25,7 +27,7 @@ export const getServerSideProps = withPageAuthRequired({
     getServerSideProps: async ({ query }) => {
         const team = String(query.team) || "NYJ";
         let season = Number(query.season) || 2022;
-        let teamParsed: IPlayerUsageStats[];
+        let teamParsed: ITeamPersonnelStats[];
 
         let weeks =
             (query.weeks === "none"
@@ -36,8 +38,8 @@ export const getServerSideProps = withPageAuthRequired({
             ? []
             : (query.downs as string)?.split(",").map(Number)) || [1, 2, 3, 4];
 
-        const teamSubRes = await prisma.player_usage_by_team.groupBy({
-            by: ["posteam", "player_id", "position", "gsis_id"],
+        const teamSubRes = await prisma.team_personnel_data.groupBy({
+            by: ["posteam", "offense_grouping"],
             where: {
                 season: season,
                 week: {
@@ -49,34 +51,18 @@ export const getServerSideProps = withPageAuthRequired({
                 posteam: team,
             },
             _sum: {
-                target: true,
-                reception: true,
-                receiving_touchdown: true,
+                snap_ct: true,
+                epa: true,
+                passing_snap: true,
+                rushing_snap: true,
                 passing_yards: true,
-                yards_after_catch: true,
-                air_yards: true,
-                redzone_target: true,
-                redzone_reception: true,
-                endzone_target: true,
-                endzone_reception: true,
-                receiving_first_down: true,
-                receiving_epa: true,
-                rush: true,
+                pass_touchdown: true,
+                pass_epa: true,
                 rushing_yards: true,
                 rush_touchdown: true,
-                stacked_box_rush: true,
-                redzone_carry: true,
-                redzone_rush_td: true,
-                goal_to_go_carry: true,
-                goalline_carry: true,
-                goalline_rush_td: true,
-                fumble: true,
-                tackled_for_loss: true,
-                rushing_first_down: true,
                 rush_epa: true,
                 week_count: true,
-                total_team_target: true,
-                total_team_rushes: true,
+                total_game_snaps: true,
             },
             _count: {
                 game_id: true,
@@ -110,7 +96,7 @@ export const getServerSideProps = withPageAuthRequired({
 });
 
 interface PlayerProps {
-    teamData: IPlayerUsageStats[];
+    teamData: ITeamPersonnelStats[];
 }
 
 const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
@@ -120,192 +106,77 @@ const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
     const [downFilter, setDownFilter] = useState([1, 2, 3, 4]);
     const [selectedSeason, setSelectedSeason] = useState(query.season || 2022);
 
-    const [playerTargets, setPlayerTargets] = useState(props.teamData);
-    const [playerRushes, setPlayerRushes] = useState(props.teamData);
-    const [recChartView, setRecChartView] = useState("all");
-    const [recChartDataOne, setRecChartDataOne] = useState("target");
-    const [recChartDataTwo, setRecChartDataTwo] = useState("reception");
-    const [rushChartView, setRushChartView] = useState("all");
-    const [rushChartDataOne, setRushChartDataOne] = useState("rush");
-    const [rushChartDataTwo, setRushChartDataTwo] = useState("");
+    const [personnelChartView, setPersonnelChartView] = useState("frequency");
+    const [personnelChartDataOne, setPersonnelChartDataOne] =
+        useState("snap_ct");
+    const [personnelChartDataTwo, setPersonnelChartDataTwo] = useState("");
+    const [aggPersonnel, setAggPersonnel] = useState(props.teamData);
 
     useEffect(() => {
-        // targets
-        let sumOfTargets: number;
-        let finalTargets: IPlayerUsageStats[];
+        let sumOfSnaps: number;
+        let finalPersonnel: ITeamPersonnelStats[];
 
-        if (recChartView === "rz") {
-            sumOfTargets = props.teamData.reduce((i, obj) => {
-                return i + obj.redzone_target;
-            }, 0);
+        sumOfSnaps = props.teamData.reduce((i, obj) => {
+            return i + obj.snap_ct;
+        }, 0);
 
-            finalTargets = props.teamData
-                .filter((group) => {
-                    return group.redzone_target !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        target_metric: group.redzone_target,
-                        total_team_target: sumOfTargets,
-                    };
-                });
+        finalPersonnel = props.teamData.filter((group) => {
+            return group.snap_ct !== 0;
+        });
 
-            finalTargets.sort((a, b) => {
-                return b.redzone_target - a.redzone_target;
-            });
+        finalPersonnel.forEach((group) => {
+            return { ...group, total_game_snaps: sumOfSnaps };
+        });
 
-            setPlayerTargets(finalTargets);
-        } else if (recChartView === "ez") {
-            sumOfTargets = props.teamData.reduce((i, obj) => {
-                return i + obj.endzone_target;
-            }, 0);
+        finalPersonnel.sort((a, b) => {
+            return b.snap_ct - a.snap_ct;
+        });
 
-            finalTargets = props.teamData
-                .filter((group) => {
-                    return group.endzone_target !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        target_metric: group.endzone_target,
-                        total_team_target: sumOfTargets,
-                    };
-                });
-
-            finalTargets.sort((a, b) => {
-                return b.endzone_target - a.endzone_target;
-            });
-
-            setPlayerTargets(finalTargets);
-        } else {
-            sumOfTargets = props.teamData.reduce((i, obj) => {
-                return i + obj.target;
-            }, 0);
-
-            finalTargets = props.teamData
-                .filter((group) => {
-                    return group.target !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        target_metric: group.target,
-                        total_team_target: sumOfTargets,
-                    };
-                });
-
-            finalTargets.sort((a, b) => {
-                return b.target - a.target;
-            });
-        }
-        setPlayerTargets(finalTargets);
-
-        // rushes
-        let sumOfRushes: number;
-        let finalRushes: IPlayerUsageStats[];
-
-        if (rushChartView === "rz") {
-            sumOfRushes = props.teamData.reduce((i, obj) => {
-                return i + obj.redzone_carry;
-            }, 0);
-
-            finalRushes = props.teamData
-                .filter((group) => {
-                    return group.redzone_carry !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        rush_metric: group.redzone_carry,
-                        total_team_rushes: sumOfRushes,
-                    };
-                });
-
-            finalRushes.sort((a, b) => {
-                return b.redzone_carry - a.redzone_carry;
-            });
-        } else if (rushChartView === "ez") {
-            sumOfRushes = props.teamData.reduce((i, obj) => {
-                return i + obj.goalline_carry;
-            }, 0);
-
-            finalRushes = props.teamData
-                .filter((group) => {
-                    return group.goalline_carry !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        rush_metric: group.goalline_carry,
-                        total_team_rushes: sumOfRushes,
-                    };
-                });
-
-            finalRushes.sort((a, b) => {
-                return b.goalline_carry - a.goalline_carry;
-            });
-        } else {
-            sumOfRushes = props.teamData.reduce((i, obj) => {
-                return i + obj.rush;
-            }, 0);
-
-            finalRushes = props.teamData
-                .filter((group) => {
-                    return group.rush !== 0;
-                })
-                .map((group) => {
-                    return {
-                        ...group,
-                        rush_metric: group.rush,
-                        total_team_rushes: sumOfRushes,
-                    };
-                });
-
-            finalRushes.sort((a, b) => {
-                return b.rush - a.rush;
-            });
-        }
-        setPlayerRushes(finalRushes);
-    }, [props.teamData, weekFilter, downFilter, recChartView, rushChartView]);
+        setAggPersonnel(finalPersonnel);
+    }, []);
 
     useEffect(() => {
-        if (recChartView === "all") {
-            setRecChartDataOne("target");
-            setRecChartDataTwo("reception");
-        } else if (recChartView === "rz") {
-            setRecChartDataOne("redzone_target");
-            setRecChartDataTwo("redzone_reception");
-        } else if (recChartView === "ez") {
-            setRecChartDataOne("endzone_target");
-            setRecChartDataTwo("endzone_reception");
-        }
-    }, [recChartView]);
+        let sumOfSnaps: number;
+        let finalPersonnel: ITeamPersonnelStats[];
+
+        sumOfSnaps = props.teamData.reduce((i, obj) => {
+            return i + obj.snap_ct;
+        }, 0);
+
+        finalPersonnel = props.teamData
+            .filter((group) => {
+                return group.snap_ct !== 0;
+            })
+            .map((group) => ({ ...group, total_game_snaps: sumOfSnaps }));
+
+        finalPersonnel.sort((a, b) => {
+            return b.snap_ct - a.snap_ct;
+        });
+
+        setAggPersonnel(finalPersonnel);
+    }, [props.teamData, weekFilter, downFilter, personnelChartView]);
 
     useEffect(() => {
-        if (rushChartView === "all") {
-            setRushChartDataOne("rush");
-            setRushChartDataTwo("");
-        } else if (rushChartView === "rz") {
-            setRushChartDataOne("redzone_carry");
-            setRushChartDataTwo("redzone_rush_td");
-        } else if (rushChartView === "ez") {
-            setRushChartDataOne("goalline_carry");
-            setRushChartDataTwo("goalline_rush_td");
+        if (personnelChartView === "all") {
+            setPersonnelChartDataOne("snap_ct");
+            setPersonnelChartDataTwo("");
+        } else if (personnelChartView === "rz") {
+            setPersonnelChartDataOne("passing_yards");
+            setPersonnelChartDataTwo("rushing_yards");
         }
-    }, [rushChartView]);
+    }, [personnelChartView]);
 
     return (
-        <div className={styles.usagePageContainer}>
+        <div className={styles.personnelPageContainer}>
             <Head>
-                <title>Team Usage Stats</title>
-                <meta name="description" content="Team Usage Stats" />
+                <title>Team Personnel Stats</title>
+                <meta name="description" content="Team Personnel Stats" />
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width, height=device-height"
                 />
             </Head>
-            <div className={styles.usagePage}>
+            <div className={styles.personnelPage}>
                 <div className={styles.selectorTrayContainer}>
                     <SelectorTray
                         handleWeekFilters={setWeekFilter}
@@ -314,35 +185,24 @@ const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
                         handleSeason={setSelectedSeason}
                         handleDownFilters={setDownFilter}
                         downFilter={downFilter}
-                        phaseUrl={""}
+                        phaseUrl=""
                         showStatSel={false}
                         statOption={""}
                         categories={""}
                     />
                 </div>
+
                 <UsageInfo
-                    playerData={playerTargets}
-                    columns={playerUsageColumns}
-                    barDataOne={recChartDataOne}
-                    barDataTwo={recChartDataTwo}
-                    headerTitle="team targets"
-                    altOptionOne="All"
-                    altOptionTwo="Red Zone"
-                    altOptionThree="End Zone"
-                    changeView={setRecChartView}
-                    dataKey="player_id"
-                />
-                <UsageInfo
-                    playerData={playerRushes}
-                    columns={playerRushUsageColumns}
-                    barDataOne={rushChartDataOne}
-                    barDataTwo={rushChartDataTwo}
-                    headerTitle="team rushes"
-                    altOptionOne="All"
-                    altOptionTwo="Red Zone"
-                    altOptionThree="Goal Line"
-                    changeView={setRushChartView}
-                    dataKey="player_id"
+                    playerData={aggPersonnel}
+                    columns={teamPersonnelGroupingColumns}
+                    barDataOne={personnelChartDataOne}
+                    barDataTwo={personnelChartDataTwo}
+                    headerTitle="personnel usage"
+                    altOptionOne="Frequency"
+                    altOptionTwo="Success"
+                    altOptionThree=""
+                    changeView={setPersonnelChartView}
+                    dataKey="offense_grouping"
                 />
                 <TeamLinkFooter />
             </div>
--- pages/teams/[team]/personnel.tsx
+++ pages/teams/[team]/personnel.tsx
@@ -20,101 +20,102 @@ export const getServerSideProps = withPageAuthRequired({
     getServerSideProps: async ({ query }) => {
         const team = String(query.team) || "NYJ";
         let season = Number(query.season) || 2022;
+        let teamParsed: ITeamPersonnelStats[];
 
-        let teamQueryResponse = await prisma.team_personnel_data.findMany({
+        let weeks =
+            (query.weeks === "none"
+                ? []
+                : (query.weeks as string)?.split(",").map(Number)) ||
+            regSeasonWeeks;
+        let downs = (query.downs === ""
+            ? []
+            : (query.downs as string)?.split(",").map(Number)) || [1, 2, 3, 4];
+
+        const teamSubRes = await prisma.team_personnel_data.groupBy({
+            by: ["posteam", "offense_grouping"],
             where: {
                 season: season,
-                posteam: team,
                 week: {
-                    in: regSeasonWeeks,
+                    in: weeks || [0],
                 },
+                down: {
+                    in: downs,
+                },
+                posteam: team,
+            },
+            _sum: {
+                snap_ct: true,
+                epa: true,
+                passing_snap: true,
+                rushing_snap: true,
+                passing_yards: true,
+                pass_touchdown: true,
+                pass_epa: true,
+                rushing_yards: true,
+                rush_touchdown: true,
+                rush_epa: true,
+                week_count: true,
+                total_game_snaps: true,
+            },
+            _count: {
+                game_id: true,
+            },
+            orderBy: {
+                posteam: "asc",
             },
         });
 
-        const playerData: ITeamPersonnelStats[] =
-            parseBigInt(teamQueryResponse);
+        teamParsed = parseBigInt(teamSubRes);
+
+        const flat = (obj: any, out: any) => {
+            Object.keys(obj).forEach((key) => {
+                if (typeof obj[key] == "object") {
+                    out = flat(obj[key], out); //recursively call for nesteds
+                } else {
+                    out[key] = obj[key]; //direct assign for values
+                }
+            });
+            return out;
+        };
+
+        let teamData = teamParsed.map((team) => flat(team, {}));
 
         return {
             props: {
-                players: parseBigInt(playerData),
+                teamData: teamData,
             },
         };
     },
 });
 
-interface PersonnelProps {
-    players: ITeamPersonnelStats[];
+interface PlayerProps {
+    teamData: ITeamPersonnelStats[];
 }
 
-const TeamPersonnel: React.FunctionComponent<PersonnelProps> = ({
-    ...props
-}) => {
+const PlayerWeeks: React.FunctionComponent<PlayerProps> = ({ ...props }) => {
     const router = useRouter();
     const { query } = router;
-    const [aggPersonnel, setAggPersonnel] = useState(props.players);
-    const columns = teamPersonnelGroupingColumns;
-    const [selectedSeason, setSelectedSeason] = useState(query.season || 2022);
     const [weekFilter, setWeekFilter] = useState(regSeasonWeeks);
     const [downFilter, setDownFilter] = useState([1, 2, 3, 4]);
+    const [selectedSeason, setSelectedSeason] = useState(query.season || 2022);
+
     const [personnelChartView, setPersonnelChartView] = useState("frequency");
     const [personnelChartDataOne, setPersonnelChartDataOne] =
         useState("snap_ct");
     const [personnelChartDataTwo, setPersonnelChartDataTwo] = useState("");
+    const [aggPersonnel, setAggPersonnel] = useState(props.teamData);
 
     useEffect(() => {
-        if (query.weeks !== undefined && query.weeks !== "") {
-            const selectedWeeks = (query.weeks as string)
-                ?.split(",")
-                .map(Number);
+        let sumOfSnaps: number;
+        let finalPersonnel: ITeamPersonnelStats[];
 
-            setWeekFilter(selectedWeeks);
-        } else if (query.weeks === "") {
-            setWeekFilter([]);
-        }
-
-        if (query.downs !== undefined && query.downs !== "") {
-            const selecteddowns = (query.downs as string)
-                ?.split(",")
-                .map(Number);
-
-            setDownFilter(selecteddowns);
-        } else if (query.downs === "") {
-            setDownFilter([]);
-        }
-
-        const currWeekData = props.players.filter((player) =>
-            weekFilter.includes(Number.parseInt(player.week.toString()))
-        );
-
-        const reducedPersonnel: Array<ITeamPersonnelStats> =
-            aggregatePersonnelStats(
-                currWeekData,
-                "snap_ct",
-                "passing_snap",
-                "rushing_snap",
-                "passing_yards",
-                "pass_touchdown",
-                "rushing_yards",
-                "rush_touchdown",
-                "total_game_snaps",
-                "week",
-                "down",
-                "week_count",
-                "season",
-                "epa",
-                "pass_epa",
-                "rush_epa"
-            );
-
-        const sumOfSnaps = reducedPersonnel.reduce((i, obj) => {
+        sumOfSnaps = props.teamData.reduce((i, obj) => {
             return i + obj.snap_ct;
         }, 0);
 
-        const finalPersonnel: ITeamPersonnelStats[] = reducedPersonnel.filter(
-            (group) => {
-                return group.snap_ct !== 0;
-            }
-        );
+        finalPersonnel = props.teamData.filter((group) => {
+            return group.snap_ct !== 0;
+        });
 
         finalPersonnel.forEach((group) => {
             return { ...group, total_game_snaps: sumOfSnaps };
@@ -128,48 +129,14 @@ const TeamPersonnel: React.FunctionComponent<PersonnelProps> = ({
     }, []);
 
     useEffect(() => {
-        if (personnelChartView === "all") {
-            setPersonnelChartDataOne("snap_ct");
-            setPersonnelChartDataTwo("");
-        } else if (personnelChartView === "rz") {
-            setPersonnelChartDataOne("passing_yards");
-            setPersonnelChartDataTwo("rushing_yards");
-        }
-    }, [personnelChartView]);
+        let sumOfSnaps: number;
+        let finalPersonnel: ITeamPersonnelStats[];
 
-    useEffect(() => {
-        const filteredPlayers = props.players
-            .filter((player) =>
-                weekFilter.includes(Number.parseInt(player.week.toString()))
-            )
-            .filter((player) =>
-                downFilter.includes(Number.parseInt(player.down.toString()))
-            );
-
-        const reducedPersonnel = aggregatePersonnelStats(
-            filteredPlayers,
-            "snap_ct",
-            "passing_snap",
-            "rushing_snap",
-            "passing_yards",
-            "pass_touchdown",
-            "rushing_yards",
-            "rush_touchdown",
-            "total_game_snaps",
-            "week",
-            "down",
-            "week_count",
-            "season",
-            "epa",
-            "pass_epa",
-            "rush_epa"
-        );
-
-        const sumOfSnaps = reducedPersonnel.reduce((i, obj) => {
+        sumOfSnaps = props.teamData.reduce((i, obj) => {
             return i + obj.snap_ct;
         }, 0);
 
-        const finalPersonnel: ITeamPersonnelStats[] = reducedPersonnel
+        finalPersonnel = props.teamData
             .filter((group) => {
                 return group.snap_ct !== 0;
             })
@@ -180,7 +147,17 @@ const TeamPersonnel: React.FunctionComponent<PersonnelProps> = ({
         });
 
         setAggPersonnel(finalPersonnel);
-    }, [weekFilter, downFilter]);
+    }, [props.teamData, weekFilter, downFilter, personnelChartView]);
+
+    useEffect(() => {
+        if (personnelChartView === "all") {
+            setPersonnelChartDataOne("snap_ct");
+            setPersonnelChartDataTwo("");
+        } else if (personnelChartView === "rz") {
+            setPersonnelChartDataOne("passing_yards");
+            setPersonnelChartDataTwo("rushing_yards");
+        }
+    }, [personnelChartView]);
 
     return (
         <div className={styles.personnelPageContainer}>
@@ -210,7 +187,7 @@ const TeamPersonnel: React.FunctionComponent<PersonnelProps> = ({
 
                 <UsageInfo
                     playerData={aggPersonnel}
-                    columns={columns}
+                    columns={teamPersonnelGroupingColumns}
                     barDataOne={personnelChartDataOne}
                     barDataTwo={personnelChartDataTwo}
                     headerTitle="personnel usage"
@@ -226,4 +203,4 @@ const TeamPersonnel: React.FunctionComponent<PersonnelProps> = ({
     );
 };
 
-export default TeamPersonnel;
+export default PlayerWeeks;
